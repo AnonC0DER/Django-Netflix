8f97269517179712d8d6d47c351a3d37767a909a
Hesam N <AnonCODER@tutanota.com>
2022-04-03 19:36:47 +0430

    URL Routing

--- djangoNetflix/playlists/urls.py
+++ djangoNetflix/playlists/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 from playlists.views import FeaturedPlaylistListView, TVShowListView, MovieListView
 
+'''
+It's better to use IDs in big projects like Netflix.
+Databases do really well with indexing integers.
+'''
+
 urlpatterns = [
+    path('', FeaturedPlaylistListView.as_view()),
+    path('media/<int:id>/', FeaturedPlaylistListView.as_view()),
+    
     path('movies/', MovieListView.as_view()),
+    path('movies/<slug:slug>/', MovieListView.as_view()),
+    
     path('shows/', TVShowListView.as_view()),
-    path('', FeaturedPlaylistListView.as_view())
+    path('shows/<slug:slug>/', TVShowListView.as_view()),
+    path('shows/<slug:slug>/seasons/', TVShowListView.as_view()),
+    path('shows/<slug:showSlug>/seasons/<slug:seasonsSlug>/', TVShowListView.as_view()),
 ]
